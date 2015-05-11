5e510c20fe153e664ff3b98708b3aeb208d21069
Fajar Nugraha <FajarNugraha@Fajars-MacBook-Pro.local>
2015-05-11 17:04:36 +0700

    Initial Commit

---
+++ Jojonomic/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Jojonomic
+//
+//  Created by Fajar Nugraha on 5/11/15.
+//  Copyright (c) 2015 Fajar Nugraha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Jojonomic/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Jojonomic
+//
+//  Created by Fajar Nugraha on 5/11/15.
+//  Copyright (c) 2015 Fajar Nugraha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
